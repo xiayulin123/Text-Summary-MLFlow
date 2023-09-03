22f0d3a3f8729e7f87f7bd5dbfb3115903d6f58f
xiayulin123 <xiayulinsky@gmail.com>
2023-09-03 16:27:40 -0400

    make some commits

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from textSummary.logging import logger
+from textSummary.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
